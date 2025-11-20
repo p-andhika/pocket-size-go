9c53974ab44feb89634edf1f158451d323fd3bc3
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-20 16:32:29 +0700

    refactor: extract greeting logic into separate function

--- hello/main.go
+++ hello/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	greeting := greet()
+	fmt.Println(greeting)
+}
+
+func greet() string {
+	// return simple greeting message
+	return "Hello world"
 }

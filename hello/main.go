931e64e3b35e9225da131dc7d613d96039f62973
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-21 14:37:33 +0700

    refactor: replace switch with map for greetings

--- hello/main.go
+++ hello/main.go
@@ -7,15 +7,22 @@ func main() {
 	fmt.Println(greeting)
 }
 
+// language represent the language's code
 type language string
 
+// phrasebook holds greeting for each supported language
+var phrasebook = map[language]string{
+	"en": "Hello world",
+	"id": "Halo dunia",
+	"fr": "Bonjour le monde",
+}
+
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello world"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		return ""
+	greeting, ok := phrasebook[l]
+
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+
+	return greeting
 }

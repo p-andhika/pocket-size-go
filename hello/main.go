6a16bfbfbfa654e2befc6956a969005b7fa9822a
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-21 14:28:21 +0700

    feat: add multilingual support to greeting function

--- hello/main.go
+++ hello/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("en")
 	fmt.Println(greeting)
 }
 
-func greet() string {
-	// return simple greeting message
-	return "Hello world"
+type language string
+
+func greet(l language) string {
+	switch l {
+	case "en":
+		return "Hello world"
+	case "fr":
+		return "Bonjour le monde"
+	default:
+		return ""
+	}
 }

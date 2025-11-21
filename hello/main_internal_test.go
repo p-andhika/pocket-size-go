bdd656092644b08790332a75843003875a486a13
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-21 14:32:44 +0700

    test: add language-specific test cases for greet function

--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -2,10 +2,35 @@ package main
 
 import "testing"
 
-func TestGreet(t *testing.T) {
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
 	want := "Hello world"
 
-	input := greet()
+	input := greet(lang)
+
+	if input != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, input)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+
+	input := greet(lang)
+
+	if input != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, input)
+	}
+}
+
+func TestGreet_Indonesia(t *testing.T) {
+	lang := language("id")
+	want := ""
+
+	input := greet(lang)
 
 	if input != want {
 		// mark this test as failed
